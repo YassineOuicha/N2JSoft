c67ec97a23952c38bfd8482b0d7d46b09ffbe2c3
Yassine Ouicha Hammou <yassineouichahammou@gmail.com>
2025-12-13 13:28:34 +0100

    User repository interface

---
+++ backend/src/WebApi/Application/Interfaces/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using WebApi.Domain.Entities;
+
+namespace WebApi.Application.Interfaces;
+
+internal interface IUserRepository
+{
+    Task<User?> GetUserByIdAsync(Guid userId, CancellationToken ct);
+    Task<IReadOnlyList<User>> GetUsersAsync(bool onlyActive, CancellationToken ct);
+    Task AddUserAsync(User user, CancellationToken ct);
+    Task UpdateUserAsync(User user, CancellationToken ct);
+    Task DeleteUserAsync(Guid userId, CancellationToken ct);
+}
