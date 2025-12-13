be4c83b77ace98f93ef1f741a5c0383734dcf339
Yassine Ouicha Hammou <yassineouichahammou@gmail.com>
2025-12-13 15:48:49 +0100

    Expense reports repository implementation

---
+++ backend/src/WebApi/Infrastructure/Repositories/ExpenseReportRepository.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApi.Application.Interfaces;
+using WebApi.Domain.Entities;
+using WebApi.Infrastructure.Persistence;
+
+namespace WebApi.Infrastructure.Repositories;
+
+internal sealed class ExpenseReportRepository(AppDbContext db): IExpenseReportRepository
+{
+    public async Task<ExpenseReport?> GetByIdAsync(Guid expenseReportId, CancellationToken ct)
+    {
+        return await db.ExpenseReports
+            .AsNoTracking()
+            .FirstOrDefaultAsync(er => er.Id == expenseReportId, ct);
+    }
+
+    public async Task<bool> ExistsForUserMonthAsync(Guid userId, int year, int month, CancellationToken ct)
+    {
+        return  await db.ExpenseReports
+            .AnyAsync(er => er.UserId == userId && er.Year == year && er.Month == month, ct);
+    }
+
+    public async Task AddAsync(ExpenseReport expenseReport, CancellationToken ct)
+    {
+        await db.ExpenseReports.AddAsync(expenseReport, ct);
+        await db.SaveChangesAsync(ct);
+    }
+
+    public async Task SaveChangesAsync(CancellationToken ct)
+    {
+        await db.SaveChangesAsync(ct);
+    }
+
+    public async Task DeleteAsync(ExpenseReport expenseReport, CancellationToken ct)
+    {
+        var report = db.ExpenseReports.FirstOrDefault(er => er.Id == expenseReport.Id);
+        if (report is not null)
+        {
+            db.ExpenseReports.Remove(report);
+            await db.SaveChangesAsync(ct);
+        }
+    }
+}
