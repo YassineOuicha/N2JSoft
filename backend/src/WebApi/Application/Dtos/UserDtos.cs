5650bb60e4c8efa30976086a8eb62e3db8a3fd46
Yassine Ouicha Hammou <yassineouichahammou@gmail.com>
2025-12-13 13:08:37 +0100

    Application layer: user dtos

---
+++ backend/src/WebApi/Application/Dtos/UserDtos.cs
@@ -0,0 +1,43 @@
+﻿namespace WebApi.Application.Dtos;
+
+internal sealed record UserListItemDto(
+    Guid Id,
+    string FirstName,
+    string LastName,
+    bool IsActive,
+    int MonthlyExpenseLimit
+);
+
+internal sealed record UserDetailDto(
+    Guid Id,
+    string FirstName,
+    string LastName,
+    bool IsActive,
+    int MonthlyExpenseLimit,
+    string Street,
+    string City,
+    string PostalCode,
+    string Country
+);
+
+internal sealed record CreateUserDto(
+    string FirstName,
+    string LastName,
+    string Street,
+    string City,
+    string PostalCode,
+    string Country,
+    int MonthlyExpenseLimit,
+    bool IsActive
+);
+
+internal sealed record UpdateUserDto(
+    string FirstName,
+    string LastName,
+    string Street,
+    string City,
+    string PostalCode,
+    string Country,
+    int MonthlyExpenseLimit,
+    bool IsActive
+);
