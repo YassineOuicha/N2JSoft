0b121726f00712b635f5ab5ad61eccb4ca615264
Yassine Ouicha Hammou <yassineouichahammou@gmail.com>
2025-12-13 17:11:55 +0100

    DI

--- backend/src/WebApi/Infrastructure/Api/DependencyInjectionExtensions.cs
+++ backend/src/WebApi/Infrastructure/Api/DependencyInjectionExtensions.cs
@@ -1,9 +1,21 @@
+using WebApi.Application.Interfaces;
+using WebApi.Application.UseCases;
+using WebApi.Infrastructure.Repositories;
+
 namespace WebApi.Infrastructure.Api;
 
 internal static class DependencyInjectionExtensions
 {
     public static IServiceCollection AddPersistence(this IServiceCollection services)
     {
+        services.AddScoped<IUserRepository, UserRepository>();
+        services.AddScoped<IExpenseReportRepository, ExpenseReportRepository>();
+        services.AddScoped<IExpenseRepository, ExpenseRepository>();
+
+        services.AddScoped<UserService>();
+        services.AddScoped<ExpenseReportService>();
+        services.AddScoped<ExpenseService>();
+        
         return services;
     }
 }
