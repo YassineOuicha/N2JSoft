8b5cc601658fee158f78fabb497dd55fc1a4dde9
Yassine Ouicha Hammou <yassineouichahammou@gmail.com>
2025-12-13 15:19:06 +0100

    Updating expense reports table with properties validations

---
+++ backend/src/WebApi/Infrastructure/Persistence/ExpenseReportEntityTypeConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using WebApi.Domain.Entities;
+
+namespace WebApi.Infrastructure.Persistence;
+
+internal sealed class ExpenseReportEntityTypeConfiguration: IEntityTypeConfiguration<ExpenseReport>
+{
+    public void Configure(EntityTypeBuilder<ExpenseReport> builder)
+    {
+        builder.ToTable("expense_reports");
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.Title).HasMaxLength(200).IsRequired();
+        builder.Property(x => x.Year).IsRequired();
+        builder.Property(x => x.Month).IsRequired();
+        
+        builder.HasIndex(x => new { x.UserId, x.Year, x.Month }).IsUnique();
+    }
+}
