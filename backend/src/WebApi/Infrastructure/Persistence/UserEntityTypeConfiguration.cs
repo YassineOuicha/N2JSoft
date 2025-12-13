e7a5bb42ec7ecad8e3a067d2215e44489d275a8d
Yassine Ouicha Hammou <yassineouichahammou@gmail.com>
2025-12-13 15:12:29 +0100

    Updating users table with properties validations

--- backend/src/WebApi/Infrastructure/Persistence/UserEntityTypeConfiguration.cs
+++ backend/src/WebApi/Infrastructure/Persistence/UserEntityTypeConfiguration.cs
@@ -9,5 +9,18 @@ internal sealed class UserEntityTypeConfiguration : IEntityTypeConfiguration<Use
     public void Configure(EntityTypeBuilder<User> builder)
     {
         builder.ToTable("users");
+        builder.Property(x => x.FirstName).HasMaxLength(100).IsRequired();
+        builder.Property(x => x.LastName).HasMaxLength(100).IsRequired();
+        builder.OwnsOne(x => x.PostalAddress, owned =>
+        {
+            owned.Property(x => x.Street).HasMaxLength(200).IsRequired();
+            owned.Property(x => x.PostalCode).HasMaxLength(20).IsRequired();
+            owned.Property(x => x.City).HasMaxLength(100).IsRequired();
+            owned.Property(x => x.Country).HasMaxLength(100).IsRequired();
+        });
+
+        builder.Property(x => x.MonthlyExpenseLimit).IsRequired();
+        builder.Property(x => x.IsActive).IsRequired();
+        builder.Property(x => x.IsDeleted).IsRequired();
     }
 }
