283753f131a1d8a9edd0e81a44c49eff0c16275f
Yassine Ouicha Hammou <yassineouichahammou@gmail.com>
2025-12-14 23:13:00 +0100

    Users page

--- frontend/src/app/features/users/users-page.ts
+++ frontend/src/app/features/users/users-page.ts
@@ -1,4 +1,6 @@
-import { Component } from "@angular/core";
+import { Component, inject, OnInit } from "@angular/core";
+import { UsersService } from "../../core/services/users.service";
+import { UserListItemDto } from "../../shared/models/user.models";
 
 @Component({
   selector: "app-users-page",
@@ -6,4 +8,33 @@ import { Component } from "@angular/core";
   templateUrl: "./users-page.html",
   styleUrl: "./users-page.css",
 })
-export class UsersPage {}
+export class UsersPage implements OnInit {
+  private readonly usersService = inject(UsersService);
+  users: UserListItemDto[] = [];
+  onlyActive = true;
+  error: string | null = null;
+
+  ngOnInit(): void {
+    this.load();
+  }
+
+  load(): void {
+    this.error = null;
+    this.usersService.list(this.onlyActive).subscribe({
+      next: data => this.users = data,
+      error: () => this.error = 'Error while loading users'
+    });
+  }
+
+  toggle(): void {
+    this.onlyActive= !this.onlyActive;
+    this.load();
+  }
+
+  delete(id: string): void {
+    this.usersService.delete(id).subscribe({
+      next: () => this.load(),
+      error: ()=> this.error = 'Error while deleting user'
+    })
+  }
+}
